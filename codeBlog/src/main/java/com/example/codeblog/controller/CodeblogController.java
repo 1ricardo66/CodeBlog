eec2a8ab01be9125b3e97d348eec713a24ef0834
1ricardo66 <ricardofps66@gmail.com>
2020-03-18 13:53:28 -0300

    Start Controller

--- codeBlog/src/main/java/com/example/codeblog/controller/CodeblogController.java
+++ codeBlog/src/main/java/com/example/codeblog/controller/CodeblogController.java
@@ -1,5 +1,31 @@
 package com.example.codeblog.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.example.codeblog.model.Post;
+import com.example.codeblog.service.CodeblogService;
+
+@Controller
 public class CodeblogController {
+	
+	@Autowired
+	CodeblogService codeblogService;
+	
+	
+	@RequestMapping(value= "/posts", method = RequestMethod.GET)
+	public ModelAndView getPost() {
+		ModelAndView mv = new ModelAndView("posts");
+		List<Post> posts = codeblogService.findAll();
+		
+		mv.addObject("posts",posts);
+		
+		return mv;
+	}
 
 }
